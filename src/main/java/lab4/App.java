39299673679be5c3f1653e9474c830a05fbf1007
izotarina <irunchikt@mail.ru>
2021-12-10 23:27:21 +0300

    Scripted auto-commit on change (2021-12-10 23:27:21) by gitwatch.sh

--- src/main/java/lab4/App.java
+++ src/main/java/lab4/App.java
@@ -1,5 +1,9 @@
 package lab4;
 
+import akka.actor.ActorSystem;
+
 public class App {
-    public static void main(String[] args)
+    public static void main(String[] args) {
+        ActorSystem system = ActorSystem.create("test");
+    }
 }
