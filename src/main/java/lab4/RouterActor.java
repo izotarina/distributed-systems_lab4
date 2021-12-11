1689306d34de313dccf69ea03642896c12a63f76
izotarina <irunchikt@mail.ru>
2021-12-11 21:29:14 +0300

    Scripted auto-commit on change (2021-12-11 21:29:14) by gitwatch.sh

--- src/main/java/lab4/RouterActor.java
+++ src/main/java/lab4/RouterActor.java
@@ -11,6 +11,7 @@ public class RouterActor extends AbstractActor {
     private final ActorRef pool;
 
     public RouterActor() {
+        storage = getContext().actorOf()
         pool = getContext().actorOf(
                 new RoundRobinPool(5).props(Props.create(TestExecutorActor.class, logResultsActor)),
                 "routerForTests");
