1b80dd0742b36447a0e8b5e17dda458a60dfa527
izotarina <irunchikt@mail.ru>
2021-12-11 20:59:06 +0300

    Scripted auto-commit on change (2021-12-11 20:59:06) by gitwatch.sh

--- src/main/java/lab4/TestExecutorActor.java
+++ src/main/java/lab4/TestExecutorActor.java
@@ -3,13 +3,20 @@ package lab4;
 import akka.actor.AbstractActor;
 import akka.japi.pf.ReceiveBuilder;
 
+import javax.script.Invocable;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+
 public class TestExecutorActor extends AbstractActor {
     @Override
     public AbstractActor.Receive createReceive() {
         return ReceiveBuilder.create()
                 .match(StoreMessage.class, m -> {
-                    store.put(m.getKey(), m.getValue());
-                    System.out.println("receive message! "+m.toString());
+                    ScriptEngine engine = new
+                            ScriptEngineManager().getEngineByName("nashorn");
+                    engine.eval(jscript);
+                    Invocable invocable = (Invocable) engine;
+                    return invocable.invokeFunction(functionName, params).toString();
                 })
                 .match(GetMessage.class, req -> sender().tell(
                         new StoreMessage(req.getKey(), store.get(req.getKey())), self())
