261c41e7e2f90b160c5b60d51a546db821075c3b
izotarina <irunchikt@mail.ru>
2021-12-11 12:04:08 +0300

    Scripted auto-commit on change (2021-12-11 12:04:08) by gitwatch.sh

--- src/main/java/lab4/Test.java
+++ src/main/java/lab4/Test.java
@@ -3,5 +3,21 @@ package lab4;
 public class Test {
     private String testName;
     private String expectedResult;
-    private String
+    private int[] params;
+
+    public Test(String testName, String expectedResult, int[] params) {
+        this.testName = testName;
+        this.expectedResult = expectedResult;
+        this.params = params;
+    }
+
+    public String getTestName() {
+        return testName;
+    }
+
+    public String getExpectedResult() {
+        return expectedResult;
+    }
+
+    p
 }
