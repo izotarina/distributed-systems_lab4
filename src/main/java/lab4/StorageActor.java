8255707c97b3acc915fd4220c14c4625102e56fc
izotarina <irunchikt@mail.ru>
2021-12-11 12:23:45 +0300

    Scripted auto-commit on change (2021-12-11 12:23:45) by gitwatch.sh

--- src/main/java/lab4/StorageActor.java
+++ src/main/java/lab4/StorageActor.java
@@ -4,16 +4,17 @@ import akka.actor.AbstractActor;
 import akka.japi.pf.ReceiveBuilder;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class StorageActor extends AbstractActor {
-    private Map<String, String> store = new HashMap<>();
+    private Map<String, List<TestResult>> store = new HashMap<>();
 
     @Override
     public Receive createReceive() {
         return ReceiveBuilder.create()
-                .match(StoreMessage.class, m -> {
-                    store.put(m.getKey(), m.getValue());
+                .match(TestResult.class, m -> {
+                    store.put(m.get(), m.getValue());
                     System.out.println("receive message! "+m.toString());
                 })
                 .match(GetMessage.class, req -> sender().tell(
