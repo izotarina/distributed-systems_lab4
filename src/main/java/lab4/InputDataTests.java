fdf6e3689b5265b7d8a0f7bc2768c3f00b72a5ee
izotarina <irunchikt@mail.ru>
2021-12-11 21:33:17 +0300

    Scripted auto-commit on change (2021-12-11 21:33:17) by gitwatch.sh

--- src/main/java/lab4/InputDataTests.java
+++ src/main/java/lab4/InputDataTests.java
@@ -1,5 +1,31 @@
 package lab4;
 
 public class InputDataTests {
-    private 
+    private final String packageId;
+    private final String jsScript;
+    private final String functionName;
+    private final Test test;
+
+    public TestExecuteRequest(String packageId, String jsScript, String functionName, Test test) {
+        this.packageId = packageId;
+        this.jsScript = jsScript;
+        this.functionName = functionName;
+        this.test = test;
+    }
+
+    public String getPackageId() {
+        return packageId;
+    }
+
+    public String getJsScript() {
+        return jsScript;
+    }
+
+    public String getFunctionName() {
+        return functionName;
+    }
+
+    public Test getTest() {
+        return test;
+    }
 }
