faab9ced1e1e05581e084aa3b2b45f5cd1973944
izotarina <irunchikt@mail.ru>
2021-12-11 21:56:17 +0300

    Scripted auto-commit on change (2021-12-11 21:56:17) by gitwatch.sh

--- src/main/java/lab4/App.java
+++ src/main/java/lab4/App.java
@@ -3,6 +3,8 @@ package lab4;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
+import akka.http.javadsl.Http;
+import akka.stream.ActorMaterializer;
 
 public class App {
     public static void main(String[] args) {
@@ -10,6 +12,8 @@ public class App {
         ActorRef router = system.actorOf(
                 Props.create(RouterActor.class)
         );
-        
+
+        final Http http = Http.get(system);
+        final ActorMaterializer materializer = ActorMaterializer.create(system);
     }
 }
