c1e96771a925f555ea4ba93c3ab7273b142545e6
izotarina <irunchikt@mail.ru>
2021-12-11 21:12:11 +0300

    Scripted auto-commit on change (2021-12-11 21:12:11) by gitwatch.sh

--- src/main/java/lab4/TestExecutorActor.java
+++ src/main/java/lab4/TestExecutorActor.java
@@ -14,12 +14,11 @@ public class TestExecutorActor extends AbstractActor {
     public AbstractActor.Receive createReceive() {
         return ReceiveBuilder.create()
                 .match(TestRequest.class, m -> {
-                    int[] params = m.getTest().getParams();
                     ScriptEngine engine = new
                             ScriptEngineManager().getEngineByName(ENGINE_NAME);
                     engine.eval(m.getJsScript());
                     Invocable invocable = (Invocable) engine;
-                    return invocable.invokeFunction(m.getFunctionName(), params).toString();
+                    String result = invocable.invokeFunction(m.getFunctionName(), m.getTest().getParams()).toString();
                 })
                 .match(GetMessage.class, req -> sender().tell(
                         new StoreMessage(req.getKey(), store.get(req.getKey())), self())
