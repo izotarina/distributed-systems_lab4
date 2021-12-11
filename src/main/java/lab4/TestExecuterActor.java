dfef4d561aa3c161ba58d39e215fa6ca48193ba9
izotarina <irunchikt@mail.ru>
2021-12-11 14:05:59 +0300

    Scripted auto-commit on change (2021-12-11 14:05:59) by gitwatch.sh

--- src/main/java/lab4/TestExecuterActor.java
+++ src/main/java/lab4/TestExecuterActor.java
@@ -1,9 +1,21 @@
 package lab4;
 
+import akka.actor.AbstractActor;
 import akka.actor.ActorSystem;
+import akka.japi.pf.ReceiveBuilder;
 
 import java.util.List;
 
 public class TestExecuterActor extends ActorSystem {
-    List<TestResult>
+    @Override
+    public AbstractActor.Receive createReceive() {
+        return ReceiveBuilder.create()
+                .match(StoreMessage.class, m -> {
+                    store.put(m.getKey(), m.getValue());
+                    System.out.println("receive message! "+m.toString());
+                })
+                .match(GetMessage.class, req -> sender().tell(
+                        new StoreMessage(req.getKey(), store.get(req.getKey())), self())
+                ).build();
+    }
 }
