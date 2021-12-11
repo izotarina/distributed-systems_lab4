3f2fc44e628264b94431271bc5b038119112d7de
izotarina <irunchikt@mail.ru>
2021-12-11 12:10:26 +0300

    Scripted auto-commit on change (2021-12-11 12:10:26) by gitwatch.sh

--- src/main/java/lab4/TestRequest.java
+++ src/main/java/lab4/TestRequest.java
@@ -5,4 +5,11 @@ public class TestRequest {
     private String jsScript;
     private String functionName;
     private Test[] tests;
+
+    public TestRequest(String packageId, String jsScript, String functionName, Test[] tests) {
+        this.packageId = packageId;
+        this.jsScript = jsScript;
+        this.functionName = functionName;
+        this.tests = tests;
+    }
 }
