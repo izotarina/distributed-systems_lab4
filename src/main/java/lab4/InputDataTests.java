209092846d550637c080db84916812d4380239c4
izotarina <irunchikt@mail.ru>
2021-12-11 21:33:37 +0300

    Scripted auto-commit on change (2021-12-11 21:33:37) by gitwatch.sh

--- src/main/java/lab4/InputDataTests.java
+++ src/main/java/lab4/InputDataTests.java
@@ -1,10 +1,12 @@
 package lab4;
 
+import java.util.List;
+
 public class InputDataTests {
     private final String packageId;
     private final String jsScript;
     private final String functionName;
-    private final Test test;
+    private final List<Test> tests;
 
     public InputDataTests(String packageId, String jsScript, String functionName, Test test) {
         this.packageId = packageId;
