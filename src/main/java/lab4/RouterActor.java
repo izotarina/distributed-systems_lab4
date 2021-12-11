69ff6f324d5d99aef6799d3c337d240a0e8d0c13
izotarina <irunchikt@mail.ru>
2021-12-11 21:20:42 +0300

    Scripted auto-commit on change (2021-12-11 21:20:42) by gitwatch.sh

--- src/main/java/lab4/RouterActor.java
+++ src/main/java/lab4/RouterActor.java
@@ -1,7 +1,18 @@
 package lab4;
 
 import akka.actor.AbstractActor;
+import akka.japi.pf.ReceiveBuilder;
 
 public class RouterActor extends AbstractActor {
-    
+    @Override
+    public Receive createReceive() {
+        return ReceiveBuilder.create()
+                .match(StoreMessage.class, m -> {
+                    store.put(m.getKey(), m.getValue());
+                    System.out.println("receive message! "+m.toString());
+                })
+                .match(GetMessage.class, req -> sender().tell(
+                        new StoreMessage(req.getKey(), store.get(req.getKey())), self())
+                ).build();
+    }
 }
